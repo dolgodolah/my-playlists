dfd0e0cb72f4b952e523c71c400aacaa9e605b27
dolgodolah <dolgodolah@gmail.com>
2021-01-12 02:30:45 +0900

    세팅

---
+++ src/main/java/com/todaymusic/TodayMusicApplication.java
@@ -0,0 +1,13 @@
+package com.todaymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todaymusic/TodayMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaymusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
