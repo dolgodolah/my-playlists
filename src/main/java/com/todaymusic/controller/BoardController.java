ecdca8acceb2651860e5ba6637a6798b96c3f44a
dolgodolah <dolgodolah@gmail.com>
2021-01-26 23:27:27 +0900

    음악(영상) 조회 구현

--- src/main/java/com/todaymusic/controller/BoardController.java
+++ src/main/java/com/todaymusic/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.todaymusic.controller;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +18,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.todaymusic.dto.MusicDTO;
 import com.todaymusic.service.MusicService;
 import com.todaymusic.service.WeatherService;
+import com.todaymusic.service.YoutubeService;
 
 
 
 @Controller
 public class BoardController {
+//	@Autowired
+//	MusicService musicService;
+//	@Autowired
+//	WeatherService weatherService;
+	
 	@Autowired
-	MusicService musicService;
-	@Autowired
-	WeatherService weatherService;
+	private MusicService musicService;
+	private WeatherService weatherService;
+	private YoutubeService youtubeService;
+	public BoardController(MusicService musicService, WeatherService weatherService, YoutubeService youtubeService) {
+		this.musicService = musicService;
+		this.weatherService = weatherService;
+		this.youtubeService = youtubeService;
+	}
 	
-	
-	
+
 	@GetMapping("/")
-	public String main(Model model) throws ParseException {
+	public String main(Model model) throws ParseException, IOException {
+		List<String> videoList = new ArrayList<String>();
 		HashMap<String, String> items = weatherService.getItemsFromApi();
+		List<MusicDTO> musicList = musicService.getMusicList("1");//items.get("PTY")
+		for(int i=0; i<musicList.size(); i++) {
+			videoList.add("http://www.youtube.com/embed/"+youtubeService.getVideoId(musicList.get(i).getTitle()+musicList.get(i).getArtist())+"?enablejsapi=1&origin=http://example.com");
+		}
+		System.out.println(videoList.get(0));
+		
 //		System.out.println(items);
 		model.addAttribute("pty", items.get("PTY"));
 		model.addAttribute("t1h", items.get("T1H"));
+		model.addAttribute("musicList",videoList);
+		
 		return "board/list";
 	}
 	
