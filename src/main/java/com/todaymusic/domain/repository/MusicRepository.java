3cb32d861a1f9cbe51e6b3df9a7b17add8718c51
dolgodolah <dolgodolah@gmail.com>
2021-01-26 23:22:55 +0900

    JPA findByPty 구현

--- src/main/java/com/todaymusic/domain/repository/MusicRepository.java
+++ src/main/java/com/todaymusic/domain/repository/MusicRepository.java
@@ -1,9 +1,12 @@
 package com.todaymusic.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.todaymusic.domain.entity.Music;
 
-public interface MusicRepository extends JpaRepository<Music, Long>{
 
+public interface MusicRepository extends JpaRepository<Music, Long>{
+	List<Music> findByPty(String pty);
 }
--- src/main/java/com/todaymusic/service/MusicService.java
+++ src/main/java/com/todaymusic/service/MusicService.java
@@ -1,9 +1,13 @@
 package com.todaymusic.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.todaymusic.domain.entity.Music;
 import com.todaymusic.domain.repository.MusicRepository;
 import com.todaymusic.dto.MusicDTO;
 
@@ -19,5 +23,23 @@ public class MusicService {
 	public Long saveMusic(MusicDTO musicDTO) {
 		return musicRepository.save(musicDTO.toEntity()).getId();
 	}
+	
+	@Transactional
+    public List<MusicDTO> getMusicList(String pty){
+    	//db에 있는 게시물 전부 찾아 boardList에 저장
+    	List<Music> musicList = musicRepository.findByPty(pty);
+    	//찾은 게시물을 담을 boardDTOList 생성
+    	List<MusicDTO> musicDTOList = new ArrayList<>();
+    	
+    	for(Music music : musicList) {
+    		MusicDTO musicDTO = MusicDTO.builder()
+    				.id(music.getId())
+    				.title(music.getTitle())
+    				.artist(music.getArtist())
+    				.build();
+    		musicDTOList.add(musicDTO);
+    	}
+    	return musicDTOList;
+    }
 	
 }
