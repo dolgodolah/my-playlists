ad27e2ef27a83a24681088181ba3447c52e30768
dolgodolah <dolgodolah@gmail.com>
2021-01-31 02:59:38 +0900

    추천리스트와 세부정보 분리

--- src/main/java/com/todaymusic/service/MusicService.java
+++ src/main/java/com/todaymusic/service/MusicService.java
@@ -26,12 +26,10 @@ public class MusicService {
 	}
 	
 	@Transactional
-    public List<MusicDTO> getMusicList(String pty){
-    	
-    	List<Music> musicList = musicRepository.findByPty(pty);//db에 해당 pty 찾아서 musicList에 저장
+	public List<MusicDTO> getMusicList(String pty){
+		List<Music> musicList = musicRepository.findByPty(pty);//db에 해당 pty 찾아서 musicList에 저장
     	//찾은 게시물을 담을 boardDTOList 생성
     	List<MusicDTO> musicDTOList = new ArrayList<>();//찾은 musicList들에 대한 정보를 담을 DTOList 생성
-    	
     	for(Music music : musicList) {
     		//유튜브 검색을 위한 정보만 필요하기 때문에 제목+가수명만 추출한다.
     		MusicDTO musicDTO = MusicDTO.builder()
@@ -42,7 +40,7 @@ public class MusicService {
     		musicDTOList.add(musicDTO);
     	}
     	return musicDTOList;
-    }
+	}
 	
 	@Transactional
 	public MusicDTO getMusic(Long id) {
