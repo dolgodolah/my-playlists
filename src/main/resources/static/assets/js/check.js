37066a08b4c826c2428c593167b4b45d696d8d6c
dolgodolah <dolgodolah@gmail.com>
2021-06-03 13:17:59 +0900

    플레이리스트/노래 삭제 기능 수정
    노래 삭제 시에도 플레이리스트가 삭제되던 현상 수정

--- src/main/resources/static/assets/js/check.js
+++ src/main/resources/static/assets/js/check.js
@@ -1,5 +1,14 @@
-function checkDeletion(){
-	const deleteForm = document.forms['deleteForm'];
+function checkPlaylistDeletion(){
+	const deleteForm = document.forms['deletePlaylistForm'];
+	if (confirm("정말 삭제하시겠습니까?")==true){
+		deleteForm.submit();
+	}else{
+		return;
+	}
+}
+
+function checkSongDeletion(){
+	const deleteForm = document.forms['deleteSongForm'];
 	if (confirm("정말 삭제하시겠습니까?")==true){
 		deleteForm.submit();
 	}else{
