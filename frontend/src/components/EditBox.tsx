1687538a03d08583868cb2d153ccd71d47c22e5e
연훈 배 <sbaru1997@naver.com>
2022-04-02 20:44:13 +0900

    feat: 편집 박스 컴포넌트 구현
    북마크와 삭제 로직은 추후 추가 구현

--- frontend/src/components/EditBox.tsx
+++ frontend/src/components/EditBox.tsx
@@ -1,22 +1,70 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import { EditBoxProps } from "../shared/Props";
+import classNames from "classnames";
+import { useState } from "react";
 
 const EditBox = ({ page }: EditBoxProps) => {
-  return (
-    <div className="edit-box__container">
-      {/* Link, pid, sid 받아오기. */}
-      {page === "/playlistDetail" ? (
-        <>
-          <Icon icon="fluent:text-bullet-list-square-edit-20-regular" />
-          <Icon icon="carbon:delete" />
-          <Icon icon="bi:bookmark-star-fill" />
-        </>
-      ) : page === "/songDetail" ? (
-        <Icon icon="carbon:music-add" />
-      ) : null}
-    </div>
-  );
+  const navigate = useNavigate();
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const setBookmark = () => {
+    // TODO: 북마크 설정 로직 추가
+    setIsBookmarked(!isBookmarked);
+  };
+  const deletePlaylist = () => {
+    const ok = window.confirm("플레이리스트를 삭제하시겠습니까?");
+    if (ok) {
+      // TODO: 플리 삭제 로직 추가
+      navigate("/");
+    }
+  };
+  const Render = () => {
+    switch (page) {
+      case "showSongs":
+      case "playSongs":
+      case "updateSong": {
+        return (
+          <>
+            <Icon
+              icon="bi:star-fill"
+              className={classNames({
+                isBookmarked,
+              })}
+              onClick={setBookmark}
+            />
+            <Link
+              to="/playlist"
+              state={{
+                page: "searchSong",
+              }}
+              className="edit_box__link"
+            >
+              <Icon icon="carbon:music-add" />
+            </Link>
+            <Icon icon="carbon:delete" onClick={deletePlaylist} />
+          </>
+        );
+      }
+      case "searchSong": {
+        return (
+          <>
+            <Link
+              to="/playlist"
+              state={{
+                page: "searchSong",
+              }}
+              className="edit_box__link"
+            >
+              <Icon icon="carbon:music-add" />
+            </Link>
+            <Icon icon="carbon:delete" onClick={deletePlaylist} />
+          </>
+        );
+      }
+    }
+    return null;
+  };
+  return <div className="edit-box__container">{Render()}</div>;
 };
 
 export default EditBox;
