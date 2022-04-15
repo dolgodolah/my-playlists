507b63265d2e230e89dde204c2427446c87ca76d
연훈 배 <sbaru1997@naver.com>
2022-04-15 23:55:10 +0900

    feat: 수정 삭제 버튼 추가

--- frontend/src/components/YoutubeVideo.tsx
+++ frontend/src/components/YoutubeVideo.tsx
@@ -1,12 +1,18 @@
-import {SongProps} from "../shared/Props";
-import {useCallback, useState} from "react";
+import { SongProps } from "../shared/Props";
+import { useCallback, useState } from "react";
 
 const YoutubeVideo = ({ song }: SongProps) => {
   const [description, setDescription] = useState(song.description);
-  const onChange = useCallback(e => {
+  const onChange = useCallback((e) => {
     setDescription(e.target.value);
   }, []);
-
+  const onClickEdit = () => {
+    console.log(description);
+    // 새로운 description 저장 로직 추가
+  };
+  const onClickDelete = () => {
+    // 노래 삭제 로직 추가
+  };
   return (
     <div className="youtube__container">
       <video
@@ -16,7 +22,19 @@ const YoutubeVideo = ({ song }: SongProps) => {
         src={song.videoId}
       ></video>
       <div className="description__container--youtube">
-        <textarea className="description__textarea" value={description} onChange={onChange}/>
+        <textarea
+          className="description__textarea"
+          value={description}
+          onChange={onChange}
+        />
+      </div>
+      <div className="button__container--youtube">
+        <span className="button__span--edit" onClick={onClickEdit}>
+          수정
+        </span>
+        <span className="button__span--delete" onClick={onClickDelete}>
+          삭제
+        </span>
       </div>
       <div className="thumbnail__container--youtube">
         <img
