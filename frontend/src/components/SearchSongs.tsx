f551672edfde41db360bc28d66cb7756006f9947
garfield-kim <dolgodolah@gmail.com>
2022-04-24 20:34:47 +0900

    노래검색 컴포넌트 적용

--- frontend/src/components/SearchSongs.tsx
+++ frontend/src/components/SearchSongs.tsx
@@ -1,12 +1,26 @@
+import React, {useCallback, useState} from "react";
+
+const onSubmit = (e: React.FormEvent) => {
+  e.preventDefault()
+  console.log("유튜브 노래 검색 api 호출")
+  // 유튜브 노래 검색 api 호출
+}
+
 const SearchSongs = () => {
+  const [keyword, setKeyword] = useState('')
+  const onChange = useCallback((e) => {
+    setKeyword(e.target.value)
+  }, []);
+
   return (
     <div className="search-song__container">
-      <form method="get" action="/playlist/search">
+      <form onSubmit={onSubmit}>
         <input
-          type="text"
           placeholder="노래 검색"
           name="keyword"
           className="search-song__input"
+          value={keyword}
+          onChange={onChange}
         />
       </form>
     </div>
