a29a58bcb9bd6eb4aa17912d373c732d5d0e7907
Yeunhun Bae <sbaru1997@naver.com>
2022-04-26 18:04:40 +0900

    refactor: 노래 검색 결과 컴포넌트를 노래 검색 컴포넌트에 병합

--- frontend/src/components/SearchSongs.tsx
+++ frontend/src/components/SearchSongs.tsx
@@ -1,29 +1,66 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback, useState } from "react";
+import { songs as newSongs } from "../test/user";
 
-const onSubmit = (e: React.FormEvent) => {
-  e.preventDefault()
-  console.log("유튜브 노래 검색 api 호출")
-  // 유튜브 노래 검색 api 호출
+interface SongProps {
+  id: number;
+  videoId: string;
+  title: string;
 }
 
 const SearchSongs = () => {
-  const [keyword, setKeyword] = useState('')
+  const [keyword, setKeyword] = useState("");
+  const [songs, setSongs] = useState([]) as Array<any>;
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("유튜브 노래 검색 api 호출");
+    // 유튜브 노래 검색 api 호출
+    setSongs(newSongs);
+  };
   const onChange = useCallback((e) => {
-    setKeyword(e.target.value)
+    setKeyword(e.target.value);
   }, []);
+  const addSong = (song: SongProps) => {
+    console.log(song);
+    // TODO: 노래 추가 로직
+  };
 
   return (
-    <div className="search-song__container">
-      <form onSubmit={onSubmit}>
-        <input
-          placeholder="노래 검색"
-          name="keyword"
-          className="search-song__input"
-          value={keyword}
-          onChange={onChange}
-        />
-      </form>
-    </div>
+    <>
+      <div className="search-song__container">
+        <form onSubmit={onSubmit}>
+          <input
+            placeholder="노래 검색"
+            name="keyword"
+            className="search-song__input"
+            value={keyword}
+            onChange={onChange}
+          />
+        </form>
+      </div>
+      {songs.map((song: SongProps) => (
+        <div key={song.id} className="song-search-result__container">
+          <div>
+            <img
+              className="song-search-result__image"
+              src={`http://i.ytimg.com/vi/${song.videoId}/maxresdefault.jpg`}
+              alt="youtube_thumbnail"
+            />
+          </div>
+          <div>
+            <span className="song-search-result__span">{song.title}</span>
+          </div>
+          <div>
+            <button
+              onClick={() => addSong(song)}
+              className="song-search-result__button"
+            >
+              추가
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
