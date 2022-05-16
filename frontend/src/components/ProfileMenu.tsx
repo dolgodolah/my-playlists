fbb87b810401fc38eeaeb1fe8de9715b580ace86
Yeunhun Bae <sbaru1997@naver.com>
2022-05-16 20:43:35 +0900

    feat: 닉네임 클릭 시 내 정보 이동 로직 추가

--- frontend/src/components/ProfileMenu.tsx
+++ frontend/src/components/ProfileMenu.tsx
@@ -1,15 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 interface UserProps {
   user: Record<string, any>;
 }
 
 const ProfileMenu = ({ user }: UserProps) => {
+  const navigate = useNavigate();
+
+  const onUserClick = () => {
+    navigate("/me");
+  };
   const logout = () => {
     console.log("logout");
     // 로그아웃 로직
   };
   return (
     <div className="profile-menu__container">
-      <span className="profile-menu__span">{user.nickname}</span>
+      <span className="profile-menu__span" onClick={onUserClick}>
+        {user.nickname}
+      </span>
       <span className="profile-menu__span--logout" onClick={logout}>
         로그아웃
       </span>
