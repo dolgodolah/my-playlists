c5d5f4569ba0e2fbdb828a1182688b857b4a569d
Yeunhun Bae <sbaru1997@naver.com>
2022-06-03 23:26:51 +0900

    feat: 플레이리스트 검색 api 추가

--- frontend/src/components/Playlist.tsx
+++ frontend/src/components/Playlist.tsx
@@ -1,7 +1,7 @@
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 import { PlaylistProps } from "../shared/Props";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface PageProps {
@@ -10,6 +10,7 @@ interface PageProps {
 
 const Playlist = ({ page }: PageProps) => {
   const [playlists, setPlaylists] = useState([]) as Array<any>;
+
   useEffect(() => {
     let api = "";
     switch (page) {
@@ -20,56 +21,57 @@ const Playlist = ({ page }: PageProps) => {
         api = "/all_playlists";
         break;
       case "bookmarks":
-        api = "/bookmarks"
+        api = "/bookmarks";
+        break;
+      case "search":
+        api = "/playlist/search";
         break;
     }
 
     axios
       .get(api)
       .then(function (response) {
-        const playlists: Array<any> = response.data.playlists
-        setPlaylists(playlists)
+        const playlists: Array<any> = response.data.playlists;
+        setPlaylists(playlists);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }, [])
+  }, []);
 
-  return (
-    playlists.map((playlist : PlaylistProps) => (
-      <Link key={playlist.playlistId}
-        to="/playlist"
-        state={{
-          page: "showSongs",
-        }}
-        className="playlist__link"
-      >
-        <div className="playlist__container">
-          <div className="playlist__container--left">
-            <span className="playlist__span--title">{playlist.title}</span>
-            <div>
+  return playlists.map((playlist: PlaylistProps) => (
+    <Link
+      key={playlist.playlistId}
+      to="/playlist"
+      state={{
+        page: "showSongs",
+      }}
+      className="playlist__link"
+    >
+      <div className="playlist__container">
+        <div className="playlist__container--left">
+          <span className="playlist__span--title">{playlist.title}</span>
+          <div>
             <span className="playlist__span--updatedDate">
               {playlist.updatedDate}
             </span>
-              <span className="playlist__span--author">{playlist.author}</span>
-            </div>
+            <span className="playlist__span--author">{playlist.author}</span>
           </div>
-          <div className="playlist__container--right">
-            <span className="playlist__span--amount">{playlist.songCount}</span>
-            <div className="playlist-icon__container">
-              <Icon icon="bxs:playlist" />
-            </div>
+        </div>
+        <div className="playlist__container--right">
+          <span className="playlist__span--amount">{playlist.songCount}</span>
+          <div className="playlist-icon__container">
+            <Icon icon="bxs:playlist" />
           </div>
-          <div className="hidden-background__container">
+        </div>
+        <div className="hidden-background__container">
           <span className="hidden-background__containe__span">
             <Icon icon="codicon:debug-start" />
           </span>
-          </div>
         </div>
-      </Link>
-    ))
-
-  );
+      </div>
+    </Link>
+  ));
 };
 
 export default Playlist;
