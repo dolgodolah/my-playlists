cba6d740f4c8a461e98e7a5415dc3fb7780f7567
garfield-kim <dolgodolah@gmail.com>
2022-09-30 00:23:45 +0900

    back: PlaylistRepository JPQL 적용
    - JpaRepository를 사용하면 findByUserId로 플레이리스트 조회 시 테이블 조인이 발생함.
    - 불필요한 테이블 조인이라고 생각해서 JPQL을 통해 조인 없이 조회 쿼리 실행될 수 있도록 수정.
    
    TODO:
    - 지금 당장은 복잡한 쿼리나 동적쿼리가 아니기 때문에 Querydsl을 사용하지 않았는데 그냥 공부를 위해서 Querydsl로 작성해볼까?
    - SongRepository에도 JPQL 적용하자.

--- src/main/java/com/myplaylists/repository/PlaylistRepository.java
+++ src/main/java/com/myplaylists/repository/PlaylistRepository.java
@@ -5,11 +5,14 @@ import com.myplaylists.domain.Playlist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+	@Query(value = "SELECT p FROM Playlist p WHERE p.user.id = ?1")
 	List<Playlist> findByUserId(Long userId);
+	@Query(value = "SELECT p FROM Playlist p WHERE p.user.id = ?1 AND p.title LIKE %?2%")
 	List<Playlist> findByUserIdAndTitleContaining(Long userId, String title);
 	Page<Playlist> findByVisibility(Pageable pageable, boolean visibility);
 	Page<Playlist> findByTitleContaining(Pageable pageable, String title);
