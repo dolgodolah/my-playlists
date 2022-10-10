9b45c1814fd3293a90bfc6d833637add0eb5a78a
garfield-kim <dolgodolah@gmail.com>
2022-10-11 00:50:14 +0900

    back: 카카오 API 호출 부분 KakaoClient 클래스로 분리

---
+++ src/main/java/com/myplaylists/client/KakaoClient.java
@@ -0,0 +1,76 @@
+package com.myplaylists.client;
+
+import com.google.gson.Gson;
+import com.myplaylists.dto.oauth.KakaoAccount;
+import com.myplaylists.dto.oauth.KakaoOauthDto;
+import com.myplaylists.exception.ApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+@Component
+public class KakaoClient {
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final Gson GSON = new Gson();
+    private static final Logger LOGGER = LoggerFactory.getLogger(KakaoClient.class);
+    private static final String KAKAO_ACCESS_TOKEN = "https://kauth.kakao.com/oauth/token";
+    private static final String KAKAO_USER_INFO = "https://kapi.kakao.com/v2/user/me";
+    private static final String REDIRECT_URI = "http://localhost:3000/login/kakao";
+
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
+
+    @Value("${kakao.client.secret}")
+    private String kakaoClientSecret;
+
+    /**
+     * 카카오 로그인 정보로부터 유저 정보를 가져온다.
+     */
+    public KakaoAccount getKakaoUserInfo(String code) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(KAKAO_USER_INFO))
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken(code))
+                    .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8")
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            return GSON.fromJson(response.body(), KakaoOauthDto.class).getKakaoAccount();
+        } catch (Exception e) {
+            throw new ApiException("카카오 로그인에 실패했습니다.", 500);
+        }
+    }
+
+    private String getAccessToken(String code) throws Exception {
+        try {
+            String params = new StringBuilder()
+                    .append("grant_type=authorization_code")
+                    .append("&client_id=" + kakaoApiKey)
+                    .append("&redirect_uri=" + REDIRECT_URI)
+                    .append("&client_secret=" + kakaoClientSecret)
+                    .append("&code=" + code)
+                    .toString();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(KAKAO_ACCESS_TOKEN))
+                    .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8")
+                    .POST(HttpRequest.BodyPublishers.ofString(params))
+                    .build();
+
+            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            return GSON.fromJson(response.body(), HashMap.class).get("access_token").toString();
+        } catch (Exception e) {
+            LOGGER.warn(KAKAO_ACCESS_TOKEN + " api error");
+            throw e;
+        }
+    }
+}
--- src/main/java/com/myplaylists/service/AuthService.java
+++ src/main/java/com/myplaylists/service/AuthService.java
@@ -1,48 +1,24 @@
 package com.myplaylists.service;
 
-import com.google.gson.Gson;
+import com.myplaylists.client.KakaoClient;
 import com.myplaylists.domain.User;
 import com.myplaylists.dto.auth.LoginUser;
-import com.myplaylists.dto.auth.OauthRequest;
 import com.myplaylists.dto.oauth.KakaoAccount;
-import com.myplaylists.dto.oauth.KakaoOauthDto;
-import com.myplaylists.exception.ApiException;
+import com.myplaylists.dto.oauth.OauthDto;
 import com.myplaylists.exception.InvalidEmailException;
 import com.myplaylists.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class AuthService {
 
-    private static final HttpClient CLIENT = HttpClient.newHttpClient();
-    private static final Gson GSON = new Gson();
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthService.class);
-    private static final String KAKAO_ACCESS_TOKEN = "https://kauth.kakao.com/oauth/token";
-    private static final String KAKAO_USER_INFO = "https://kapi.kakao.com/v2/user/me";
-    private static final String REDIRECT_URI = "http://localhost:3000/login/kakao";
-
     private final UserRepository userRepository;
-
-    @Value("${kakao.api.key}")
-    private String kakaoApiKey;
-
-    @Value("${kakao.client.secret}")
-    private String kakaoClientSecret;
+    private final KakaoClient kakaoClient;
 
     /**
      * 내플리스 로그인 처리
@@ -73,42 +49,6 @@ public class AuthService {
      * 내플리스 로그인을 위해 필요한 Oauth 객체를 카카오 로그인 정보로부터 가져온다.
      */
     public KakaoAccount getKakaoUserInfo(String code) {
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(KAKAO_USER_INFO))
-                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken(code))
-                    .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8")
-                    .GET()
-                    .build();
-
-            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            return GSON.fromJson(response.body(), KakaoOauthDto.class).getKakaoAccount();
-        } catch (Exception e) {
-            throw new ApiException("카카오 로그인에 실패했습니다.", 500);
-        }
-    }
-
-    private String getAccessToken(String code) throws Exception {
-        try {
-            String params = new StringBuilder()
-                    .append("grant_type=authorization_code")
-                    .append("&client_id=" + kakaoApiKey)
-                    .append("&redirect_uri=" + REDIRECT_URI)
-                    .append("&client_secret=" + kakaoClientSecret)
-                    .append("&code=" + code)
-                    .toString();
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(KAKAO_ACCESS_TOKEN))
-                    .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8")
-                    .POST(HttpRequest.BodyPublishers.ofString(params))
-                    .build();
-
-            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            return GSON.fromJson(response.body(), HashMap.class).get("access_token").toString();
-        } catch (Exception e) {
-            LOGGER.warn(KAKAO_ACCESS_TOKEN + " api error");
-            throw e;
-        }
+        return kakaoClient.getKakaoUserInfo(code);
     }
 }
