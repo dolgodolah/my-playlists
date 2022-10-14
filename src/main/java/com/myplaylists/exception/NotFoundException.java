8f45006a596070bc2d32849bb373c2cc9c112300
garfield-kim <dolgodolah@gmail.com>
2022-10-15 01:31:37 +0900

    front + back: NotFoundException(404) 추가

--- src/main/java/com/myplaylists/exception/NotFoundException.java
+++ src/main/java/com/myplaylists/exception/NotFoundException.java
@@ -3,6 +3,6 @@ package com.myplaylists.exception;
 public class NotFoundException extends ApiException {
 
     public NotFoundException(String message) {
-        super(message, 440);
+        super(message, 404);
     }
 }
