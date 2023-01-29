f6aac0b5f79e4783c9a7354af14e8b172779e1a7
garfield-kim <dolgodolah@gmail.com>
2023-01-30 01:27:51 +0900

    back: nextjs에서 export한 페이지에 컨텍스트 세팅할 수 있도록 개발 (#28)
    - Java에서 HTML 돔 파싱할 수 있도록 jsoup 라이브러리 추가
    - nextjs에서 export한 페이지들에 컨텍스트 세팅하여 내려줄 수 있도록 ViewResponse 개발
    ㄴ 페이지들마다 동적으로 필요한 데이터들 세팅
    ㄴ Spring에서 ModelAndView와 비슷한 역할

---
+++ src/main/kotlin/com/myplaylists/dto/ViewResponse.kt
@@ -0,0 +1,52 @@
+package com.myplaylists.dto
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.jsoup.Jsoup
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import java.io.IOException
+
+class ViewResponse(
+    val status: HttpStatus,
+) {
+    companion object {
+        private val mapper = jacksonObjectMapper()
+        fun ok() = ViewResponse(HttpStatus.OK)
+    }
+
+    fun render(
+        page: String,
+        context: ViewContext? = null
+    ): ResponseEntity<String> {
+        return ResponseEntity.ok()
+            .contentType(MediaType.TEXT_HTML)
+            .body(replaceHtml(page, context))
+    }
+
+    private fun replaceHtml(
+        page: String,
+        context: ViewContext?
+    ): String {
+        val viewFile = ClassPathResource("/next/$page")
+        val doc = Jsoup.parse(viewFile.inputStream, "UTF8", "")
+
+        val nextDataElement = doc.getElementById("__NEXT_DATA__") ?: throw IOException()
+        val root = mapper.readTree(nextDataElement.html())
+        val pageProps = root.at("/props/pageProps") as ObjectNode
+        val pageContext = mapper.convertValue(context, JsonNode::class.java)
+        pageProps.replace("pageContext", pageContext)
+        nextDataElement.html(mapper.writeValueAsString(root))
+
+        return doc.outerHtml()
+    }
+}
+
+class PlaylistsViewContext(
+    val page: String = "myPlaylists"
+) : ViewContext
+
+interface ViewContext
