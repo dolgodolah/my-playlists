0f96c67808133403909f03eb56ab58859d28e7ba
garfield-kim <dolgodolah@gmail.com>
2023-02-10 23:55:22 +0900

    back: 상태 코드들 object로 선언

--- src/main/kotlin/com/myplaylists/exception/Exceptions.kt
+++ src/main/kotlin/com/myplaylists/exception/Exceptions.kt
@@ -2,12 +2,15 @@ package com.myplaylists.exception
 
 import java.lang.RuntimeException
 
-private const val BAD_REQUEST = 400
-private const val NOT_AUTHORIZED = 401
-private const val NOT_FOUND = 404
-private const val YOUTUBE_API = 450
-private const val EXCEED_LIMIT = 460
-private const val SIGNUP_REQUIRED = 470
+
+object StatusCodes {
+    const val BAD_REQUEST = 400
+    const val NOT_AUTHORIZED = 401
+    const val NOT_FOUND = 404
+    const val YOUTUBE_API = 450
+    const val EXCEED_LIMIT = 460
+    const val SIGNUP_REQUIRED = 470
+}
 
 open class ApiException(
     override val message: String,
@@ -16,25 +19,27 @@ open class ApiException(
 
 class NotAuthorizedException(
     message: String = "로그인을 먼저 진행해주세요.",
-): ApiException(message, NOT_AUTHORIZED)
+): ApiException(message, StatusCodes.NOT_AUTHORIZED)
 
 class NotFoundException(
     message: String
-): ApiException(message, NOT_FOUND)
+): ApiException(message, StatusCodes.NOT_FOUND)
 
 class BadRequestException(
     message: String = "올바르지 않은 요청입니다."
-): ApiException(message, BAD_REQUEST)
+): ApiException(message, StatusCodes.BAD_REQUEST)
 
 class ExceedLimitException(
     message: String
-): ApiException(message, EXCEED_LIMIT)
+): ApiException(message, StatusCodes.EXCEED_LIMIT)
 
 class YoutubeApiException(
     message: String = "유튜브 검색에 실패했습니다. 잠시 후 다시 시도해보세요."
-): ApiException(message, YOUTUBE_API)
+): ApiException(message, StatusCodes.YOUTUBE_API)
 
 class SignupRequiredException(
     message: String = "회원가입을 먼저 진행해주세요.",
-): ApiException(message, SIGNUP_REQUIRED)
+): ApiException(message, StatusCodes.SIGNUP_REQUIRED)
+
+class AuthRequiredException(): ApiException("", StatusCodes.NOT_AUTHORIZED);
 
