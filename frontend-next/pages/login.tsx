fb266f65b69b066db3015bea175814b412cd57ff
garfield-kim <dolgodolah@gmail.com>
2023-02-12 03:35:38 +0900

    front: 모바일로 로그인 페이지 접근 시 해상도 임시처리
    - 모든 페이지를 반응형으로 만들려면 시간이 걸림.
    - 로그인 페이지라도 PC, 모바일에서 적절한 해상도로 노출될 수 있도록 임시 수정.
    - 디바이스 정보 판단하는 라이브러리 (react-device-detect) 유용할 거 같아서 일단 활용.
    ㄴ 모바일 여부(isMobile)를 백엔드에서 판단할지 프론트엔드에서 판단할지는 고민 필요.

--- frontend-next/pages/login.tsx
+++ frontend-next/pages/login.tsx
@@ -1,7 +1,13 @@
 import {LoginMenu} from "../components/Login"
 import withPageContext from "../components/hoc/withPageContext"
+import {isMobile} from "react-device-detect";
 
 const LoginPage = () => {
+
+  if (isMobile) {
+    document.body.style.width = "100%";
+  }
+
   return (
     <LoginMenu />
   )
